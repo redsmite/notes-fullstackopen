5c802da23b6b8b0a94a6c0954683e1edb29fd581
Kym Carabeo <yuudachipoikancolle@gmail.com>
2026-07-27 09:06:32 +0800

    add middleware error handling and cors

--- server/index.js
+++ server/index.js
@@ -1,30 +1,26 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const Note = require('./models/note')
 
 const app = express()
 
-app.use(express.json())
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(cors())
 app.use(express.static('dist'))
+app.use(express.json())
+app.use(requestLogger)
 
-
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(requestLogger)
 
 app.get('/',(request,response)=>{
   response.send('<h1>Hello World</h1>')
@@ -37,15 +33,46 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end() 
+      }
+    })
+    .catch(error => {
+      console.log(error)
+
+      response.status(400).send({ error: 'malformatted id' })
+    })
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  Note.findByIdAndRemove(request.params.id).then(() => {
-    response.status(204).end()
-  })
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
+
+  Note.findById(request.params.id)
+    .then(note => {
+      if (!note) {
+        return response.status(404).end()
+      }
+
+      note.content = content
+      note.important = important
+
+      return note.save().then((updatedNote) => {
+        response.json(updatedNote)
+      })
+    })
+    .catch(error => next(error))
 })
 
 const generateId = () => {
@@ -55,7 +82,7 @@ const generateId = () => {
   return String(maxId + 1)
 }
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
   if (!body.content) {
@@ -64,13 +91,34 @@ app.post('/api/notes', (request, response) => {
 
   const note = new Note({
     content: body.content,
-    important: body.important || false,
+    important: Boolean(body.important)
   })
 
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  })
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
+    .catch(error => next(error))
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 
